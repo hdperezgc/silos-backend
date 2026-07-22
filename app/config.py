15355a68d8bff4c09bf8dd55004af5ec52625438
hdperezgc <hdperez@grupocresta.com.gt>
2026-07-22 15:51:00 -0600

    Add n8n_webhook_url to Settings configuration
    Added n8n_webhook_url attribute to Settings class.

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     jwt_algorithm: str = "HS256"
     jwt_expire_minutes: int = 480
     cors_origins: str = ""
+    n8n_webhook_url: str = ""
 
     @property
     def cors_origins_list(self) -> list[str]:
